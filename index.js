f9f31cb571e371e87882d6f89a4a0a3185d463f4
DeusExMortis-Eng <69949034+DeusExMortis-Eng@users.noreply.github.com>
2022-04-22 19:30:46 -0700

    Update index.js

--- index.js
+++ index.js
@@ -96,11 +96,11 @@ Tol = await getBuffer(`https://hardianto.xyz/api/goodbye3?profile=${encodeURICom
 
 Description: ${metadata.desc}
 
-Welcome To Our Comfortable Happy😋, Sometimes Loud😜, Usually Messy🤥, Full Of Love🥰, HOME😌!!`} )
+Bienvenido a nuestro Groupo!😋, Espero que sea de su agrado😎, Has todas las amistades que gustes🥰, Cumple con las reglas 🗿, Disfruta😌!!`} )
                 } else if (anu.action == 'remove') {
                     XeonBotInc.sendMessage(anu.id, { image: Tol, contextInfo: { mentionedJid: [num] }, caption: `@${num.split("@")[0]} Left ${metadata.subject}
 
-I'm not sure if it was a goodbye charm, but it was fun while it lasted 😌✨` })
+Nunca Importo... 😌✨` })
                 }
             }
         } catch (err) {
